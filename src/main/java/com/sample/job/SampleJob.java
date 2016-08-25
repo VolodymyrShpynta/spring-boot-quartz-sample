bf9f190c0f4e91a63cf77b054fa6ef764392aab7
VolodymyrShpynta <vshpynta@cogniance.com>
2016-08-25 16:09:41 +0300

    Added DisallowConcurrentExecution annotation

--- src/main/java/com/sample/job/SampleJob.java
+++ src/main/java/com/sample/job/SampleJob.java
@@ -1,10 +1,12 @@
 package com.sample.job;
 
 import com.sample.service.SampleService;
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 
+@DisallowConcurrentExecution
 public class SampleJob implements Job {
     @Autowired
     private SampleService service;
